df2a69faee9843853134becf54cac97e4c093fca
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2021-12-24 01:03:43 +0500

    Update MovieCollection.cpp

--- MovieCollection.cpp
+++ MovieCollection.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
-//Parser
+//This file will work as our parser
 
 class MovieCollection{
 
     //methods:
     //  a)  read csv file
+
+public:
+    void readFile(){}
+
     //  b)  create an object of MovieNode class
     //  c)  Automatically inserts all the entries of that line in various fields of the 
     //      data part in the MovieNode object. Note that the data field is of type Movie
